02674810f7d231ecc1bed394845770dccd9d95f0
totobene02 <totobene02@gmail.com>
2024-01-31 11:36:54 +0100

    e2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+DHT_Unified dht(SENSOR, DHT11);
 
 void setup() {
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+   sensor_t sensor;
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  delay(5000);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 }
